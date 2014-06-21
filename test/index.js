0af1224f7338dbe8666a98ac83d0321d9e32cce8
Dominic Barnes <dbarnes@treetopllc.com>
2014-06-21 11:16:12 -0500

    using hooks-emitter instead of emitter, fixing constructor function use-case

--- test/index.js
+++ test/index.js
@@ -6,4 +6,24 @@ describe("FSM(obj)", function () {
     it("should be the Machine constructor", function () {
         assert(FSM === Machine);
     });
+
+    it("should work properly on constructors", function (done) {
+        function F() {
+            FSM.call(this);
+        }
+
+        FSM(F.prototype)
+            .state("a")
+                .on("next", "b")
+            .state("b")
+                .on("enter", function () {
+                    assert(this === inst);
+                    done();
+                });
+
+        var inst = new F();
+
+        inst.start();
+        inst.handle("next");
+    });
 });
