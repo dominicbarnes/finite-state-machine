6b41e203d502eba60e704fff748384947aad6800
Dominic Barnes <dominic@dbarnes.info>
2016-04-25 11:05:52 -0700

    wip

--- test/index.js
+++ test/index.js
@@ -1,72 +1,38 @@
-var assert = require("assert");
-var FSM = require("finite-state-machine");
-var Machine = require("finite-state-machine/lib/machine.js");
 
-describe("FSM(obj)", function () {
-    it("should be the Machine constructor", function () {
-        assert(FSM === Machine);
-    });
+'use strict';
 
-    it("should work properly on constructors", function (done) {
-        function F() {
-            this.start();
+const assert = require('chai').assert;
+const fsm = require('..');
+
+
+describe('fsm(config)', function () {
+  const empty = { states: {} };
+
+  it('should be a function', function () {
+    assert.isFunction(fsm);
+  });
+
+  it('should return a function', function () {
+    assert.isFunction(fsm(empty));
+  });
+
+  context('with config', function () {
+    const turnstyle = fsm({
+      states: {
+        locked: {
+          push: 'locked',
+          coin: 'unlocked'
+        },
+        unlocked: {
+          push: 'locked',
+          coin: 'unlocked'
         }
-
-        FSM(F.prototype)
-            .state("a")
-                .on("next", "b")
-            .state("b")
-                .enter(function () {
-                    assert(this === inst);
-                    done();
-                });
-
-        var inst = new F();
-        inst.handle("next");
+      }
     });
 
-    it("should not clobber other instances", function () {
-        function F() {
-            this.start();
-        }
-
-        FSM(F.prototype)
-            .state("a")
-                .on("next", "b")
-            .state("b");
-
-        var inst1 = new F();
-        var inst2 = new F();
-
-        inst1.handle("next");
-        assert(inst1.currentState() !== inst2.currentState(), "current states should differ");
+    it('should have the configured states', function () {
+      const t = turnstyle();
+      console.log(t.getStates());
     });
-
-    it("should preserve context properly", function () {
-        function F() {
-            this.start();
-        }
-
-        FSM(F.prototype)
-            .state("a")
-                .on("next", function () {
-                    assert(this === expected);
-                    this.transition("b");
-                })
-            .state("b")
-                .on("next", function () {
-                    assert(this === expected);
-                });
-
-        var inst1 = new F();
-        var inst2 = new F();
-
-        var expected = inst1;
-        inst1.handle("next");
-        inst1.handle("next");
-
-        expected = inst2;
-        inst2.handle("next");
-        inst2.handle("next");
-    });
+  });
 });
